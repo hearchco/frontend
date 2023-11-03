ad5afb23200318e4527de9ad0b79edbab9156711
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-11-03 19:06:05 +0100

    fix: cors missing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,8 @@
 import type { HandleFetch } from '@sveltejs/kit';
-import { env } from '$env/dynamic/private';
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
-	if (request.url.startsWith(`${env.PRIVATE_API_URL}`)) {
-		request.headers.set('Origin', `${event.request.headers.get('Host')}`);
+	if (!request.headers.has('origin')) {
+		request.headers.set('origin', event.url.origin);
 	}
-
 	return fetch(request);
 };
