89c8d9a2392c8d60d577d21086ee83bbc30ad223
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-11-09 19:13:39 +0100

    fix: tracked down why it took 2x as much on SSR to load api

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -1,7 +1,16 @@
 import type { PageLoad } from './$types';
 import { env } from '$env/dynamic/public';
 
-export const load: PageLoad = async ({ fetch, url }) => {
+export const load: PageLoad = async ({ data, fetch, url }) => {
+	// this runs on SSR (+page.server.ts)
+	if (data != null) {
+		return {
+			query: data.query,
+			results: data.results
+		};
+	}
+
+	// this runs on CSR
 	const q = url.searchParams.get('q');
 	if (q === null || q === '') {
 		return {
