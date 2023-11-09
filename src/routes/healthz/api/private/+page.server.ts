dd989d29ac9661548d0e2044f6211644fe65114b
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-11-09 17:31:23 +0100

    feat(revert): sveltekit fetch

--- src/routes/healthz/api/private/+page.server.ts
+++ src/routes/healthz/api/private/+page.server.ts
@@ -3,7 +3,7 @@ export const csr = false;
 import type { PageServerLoad } from './$types';
 import { env } from '$env/dynamic/private';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ fetch }) => {
 	const apiUrl = `${env.API_URL}/healthz`;
 	const response = await fetch(apiUrl);
 
--- src/routes/healthz/api/public/+page.ts
+++ src/routes/healthz/api/public/+page.ts
@@ -3,7 +3,7 @@ export const ssr = false;
 import type { PageLoad } from './$types';
 import { env } from '$env/dynamic/public';
 
-export const load: PageLoad = async () => {
+export const load: PageLoad = async ({ fetch }) => {
 	const apiUrl = `${env.PUBLIC_API_URL}/healthz`;
 	const response = await fetch(apiUrl);
 
