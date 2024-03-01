f9125540ab1a9c8c2e7621a0548f5f3af1e5f33f
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-03-01 19:27:40 +0100

    fix(categories): remove currently unused categories

--- src/lib/types/category.ts
+++ src/lib/types/category.ts
@@ -1,10 +1,10 @@
 export enum CategoryEnum {
 	GENERAL = 'general',
-	IMAGES = 'images',
-	INFO = 'info',
-	SCIENCE = 'science',
-	NEWS = 'news',
-	BLOG = 'blog',
-	SURF = 'surf',
-	NEWNEWS = 'newnews'
+	IMAGES = 'images'
+	// INFO = 'info',
+	// SCIENCE = 'science',
+	// NEWS = 'news',
+	// BLOG = 'blog',
+	// SURF = 'surf',
+	// NEWNEWS = 'newnews'
 }
