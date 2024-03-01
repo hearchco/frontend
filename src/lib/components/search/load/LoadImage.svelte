01c99c46c9df200405cddbd40c301923d1e3cb43
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-03-01 16:09:22 +0100

    fix(load): only 2 values for random image loading size

--- src/lib/components/search/load/LoadImage.svelte
+++ src/lib/components/search/load/LoadImage.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	const widths = [115, 150, 167];
-	const w = widths[Math.floor(Math.random() * widths.length)];
+	const widths = [64, 80];
+	// pick a random width from widths
+	const width = widths[Math.floor(Math.random() * widths.length)];
 </script>
 
 <div class="animate-pulse">
 	<div class="h-full w-full overflow-hidden rounded-lg bg-gray-300 dark:bg-gray-700">
-		<div class="h-48 w-[{w}px]"></div>
+		<div class="h-48 w-{width}"></div>
 		<!-- Image loading skeleton -->
 	</div>
 </div>
