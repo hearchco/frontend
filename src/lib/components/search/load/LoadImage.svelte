9cb0dabbbf47104a7af9108bae6516ac296a3359
Matija Kljajić <64456143+matijakljajic@users.noreply.github.com>
2024-03-02 11:43:45 +0100

    fix(load): delay + pseudorandom look

--- src/lib/components/search/load/LoadImage.svelte
+++ src/lib/components/search/load/LoadImage.svelte
@@ -1,5 +1,15 @@
+<script lang="ts">
+	// parameters
+	export let colored = false;
+</script>
+
 <div class="animate-pulse">
-	<div class="h-full w-full overflow-hidden rounded-lg bg-gray-300 dark:bg-gray-700">
+	<div
+		class:bg-hearchco-primary={colored}
+		class:bg-gray-300={!colored}
+		class:dark:bg-gray-700={!colored}
+		class="h-full w-full overflow-hidden rounded-lg"
+	>
 		<div class="h-48 w-64 object-cover"></div>
 		<!-- Image loading skeleton -->
 	</div>
