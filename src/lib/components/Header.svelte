7c1c6e8a342f0646c32b7a890422cba0ece98f80
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-03-03 18:41:13 +0100

    fix(header): mobile padding

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,7 +9,7 @@
 <header
 	class="flex flex-col justify-end h-36 items-center border-b-2 border-gray-100 dark:border-zinc-700"
 >
-	<div class="mx-4 w-full min-w-fit">
+	<div class="px-4 w-full min-w-fit">
 		<div class="md:mx-auto flex max-w-screen-md items-center">
 			<Searchbox categories={true} bind:query />
 		</div>
