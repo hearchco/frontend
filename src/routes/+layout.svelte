82a317982d429b4e46a20b473a9123e4ac3fea9f
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-03-28 20:44:39 +0100

    fix(html): faster theme picking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,19 +7,6 @@
 	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<svelte:head>
-	<script type="text/javascript">
-		if (
-			localStorage.theme === 'dark' ||
-			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-		) {
-			document.documentElement.classList.add('dark');
-		} else {
-			document.documentElement.classList.remove('dark');
-		}
-	</script>
-</svelte:head>
-
 <DarkModeToggle />
 <slot />
 <Footer />
