5cf5ddda82c6d66ae58feb285d6f56f77e64f1ce
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-04-27 13:45:16 +0200

    fix: switch to enhanced form for show more button

--- src/lib/components/search/display/images/ShowMore.svelte
+++ src/lib/components/search/display/images/ShowMore.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	// functions
-	import { fetchResultsLazily } from "$lib/functions/fetchResultsLazily";
+	import { fetchResultsLazily } from '$lib/functions/fetchResultsLazily';
 
 	// types
-	import type { ResultType } from "$lib/types/result";
+	import type { ResultType } from '$lib/types/result';
 
 	// parameters
 	export let query: string;
@@ -16,20 +16,33 @@
 	let pages: number = 2;
 
 	// functions
-	const showMore = async () => {
-		const [newOffset, newResults]: [number, ResultType[]] = await fetchResultsLazily(query, start, offset, pages, results, paramsString);
+	const handleSubmit = async () => {
+		const [newOffset, newResults]: [number, ResultType[]] = await fetchResultsLazily(
+			query,
+			start,
+			offset,
+			pages,
+			results,
+			paramsString
+		);
 		offset = newOffset;
 		results = newResults;
 	};
 </script>
 
-<!-- TODO: revert back to form for no-JS users but use enhancing to load the results lazily like below -->
-<div class="pb-5 flex flex-row justify-center">
+<form
+	on:submit|preventDefault={handleSubmit}
+	method="get"
+	action="/search"
+	class="pb-5 flex flex-row justify-center"
+>
+	<input type="hidden" name="q" value={query} />
+	<input type="hidden" name="start" value={start + offset} />
+	<input type="hidden" name="pages" value={pages} />
 	<button
-		on:click={showMore}
-		type="button"
+		type="submit"
 		class="mx-auto mb-5 h-12 w-1/5 rounded-lg overflow-hidden dark:bg-zinc-800 hover:dark:bg-zinc-700 hover:ring-2 hover:ring-hearchco-primary hover:dark:ring-hearchco-secondary text-zinc-500 dark:text-zinc-200 hover:text-hearchco-primary hover:dark:text-hearchco-secondary shadow-sm dark:shadow-none border border-gray-100 dark:border-0 duration-200 ease-in-out"
 	>
 		Show more
 	</button>
-</div>
+</form>
