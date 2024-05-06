c9d9e4d3fd494c7f71fef3e68e2de0a99cf87545
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-06 21:04:08 +0200

    feat(opensearch.xml): added POST

--- src/routes/opensearch.xml/+server.js
+++ src/routes/opensearch.xml/+server.js
@@ -24,19 +24,23 @@ function getSuggestionsUrl() {
 	return `${instanceUrl}suggestions`;
 }
 
-/** @type {import('./$types').RequestHandler} */
-export async function GET() {
+/**
+ * @param {'GET' | 'POST'} opensearchMethod - The method to use for searching
+ * @returns {string} - The OpenSearch XML body
+ */
+function getOpenSearchXml(opensearchMethod) {
 	const instanceName = env.PUBLIC_INSTANCE_NAME ?? 'Hearchco';
-	const opensearchMethod = 'GET';
 	const searchUrl = getSearchUrl();
+	// TODO: const suggestionsUrl = getSuggestionsUrl();
+
+	const searchUrlTag =
+		opensearchMethod === 'GET'
+			? `<Url rel="results" type="text/html" method="${opensearchMethod}" template="${searchUrl}?q={searchTerms}"/>`
+			: `<Url rel="results" type="text/html" method="${opensearchMethod}" template="${searchUrl}">
+				<Param name="q" value="{searchTerms}" />
+			</Url>`;
+
 	// TODO: <Url rel="suggestions" type="application/x-suggestions+json" method="{{ opensearch_method }}" template="{{ url_for('autocompleter', _external=True) }}?q={searchTerms}"/>
-	// const suggestionsUrl = getSuggestionsUrl();
-
-	// TODO: POST
-	// <Url rel="results" type="text/html" method="${opensearchMethod}" template="${instanceUrl}search">
-	// <Param name="q" value="{searchTerms}" />
-	// </Url>
-
 	const body = `
         <?xml version="1.0" encoding="utf-8"?>
         <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
@@ -45,13 +49,31 @@ export async function GET() {
             <Description>Hearchco is a distributed and fast metasearch engine that respects your privacy.</Description>
             <InputEncoding>UTF-8</InputEncoding>
             <Image type="image/png">/favicon.png</Image>
-            <Url rel="results" type="text/html" method="${opensearchMethod}" template="${searchUrl}?q={searchTerms}"/>
+            ${searchUrlTag}
             <Url rel="self" type="application/opensearchdescription+xml" method="${opensearchMethod}" template="/opensearch.xml" />
             <Query role="example" searchTerms="Hearchco" />
             <moz:SearchForm>${searchUrl}</moz:SearchForm>
         </OpenSearchDescription>
     `;
 
+	return body;
+}
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET() {
+	const opensearchMethod = 'GET';
+	const body = getOpenSearchXml(opensearchMethod);
+	return new Response(body, {
+		headers: {
+			'Content-Type': 'application/opensearchdescription+xml'
+		}
+	});
+}
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST() {
+	const opensearchMethod = 'POST';
+	const body = getOpenSearchXml(opensearchMethod);
 	return new Response(body, {
 		headers: {
 			'Content-Type': 'application/opensearchdescription+xml'
