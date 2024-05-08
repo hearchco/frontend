07df75004c865666a01d0712d0d2183275c40e82
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-09 00:17:08 +0200

    fix(infiniteloading): preload results

--- src/lib/components/results/infiniteloading/main.svelte
+++ src/lib/components/results/infiniteloading/main.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { preloadData } from '$app/navigation';
 	import { fetchAdditionalResults } from '$lib/functions/api/additionalresults';
 	import { URLSearchParams } from 'svelte/reactivity';
 
@@ -22,6 +23,11 @@
 		results = newResults;
 		nextPage = nextPage + 1;
 	}
+
+	async function preloadResults() {
+		const params = new URLSearchParams({ q: query, category, start: nextPage.toString() });
+		await fetchAdditionalResults(results, params);
+	}
 </script>
 
 <form
@@ -34,6 +40,8 @@
 	<input type="hidden" name="category" value={category} />
 	<input type="hidden" name="start" value={nextPage} />
 	<button
+		onmouseover={preloadResults}
+		onfocus={preloadResults}
 		type="submit"
 		class="h-12 w-full min-[400px]:w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-[300px] text-neutral-600 dark:text-neutral-300 hover:text-hearchco-primary hover:dark:text-hearchco-secondary shadow-lg dark:shadow-none rounded-lg hover:bg-neutral-100 hover:dark:bg-neutral-800 border-2 hover:border-hearchco-primary hover:dark:border-hearchco-secondary duration-150 ease-in-out"
 	>
