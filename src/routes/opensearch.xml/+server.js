fcc5fb965b6b1ffe456bafc0ee9b4f1143fc934a
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-10 11:39:51 +0200

    fix(opensearch.xml): formatting

--- src/routes/opensearch.xml/+server.js
+++ src/routes/opensearch.xml/+server.js
@@ -29,6 +29,7 @@ function getSearchUrl() {
  */
 function getOpenSearchXml(opensearchMethod) {
 	const instanceName = env.PUBLIC_INSTANCE_NAME ?? 'Hearchco';
+	const instanceUrl = getInstanceUrl();
 	const searchUrl = getSearchUrl();
 	// TODO: const suggestionsUrl = getSuggestionsUrl();
 
@@ -41,19 +42,19 @@ function getOpenSearchXml(opensearchMethod) {
 
 	// TODO: <Url rel="suggestions" type="application/x-suggestions+json" method="{{ opensearch_method }}" template="{{ url_for('autocompleter', _external=True) }}?q={searchTerms}"/>
 	const body = `
-        <?xml version="1.0" encoding="utf-8"?>
-        <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-            <ShortName>${instanceName}</ShortName>
-            <LongName>Hearchco metasearch engine</LongName>
-            <Description>Hearchco is a distributed and fast metasearch engine that respects your privacy.</Description>
-            <InputEncoding>UTF-8</InputEncoding>
-            <Image type="image/png">/favicon.png</Image>
-            ${searchUrlTag}
-            <Url rel="self" type="application/opensearchdescription+xml" method="${opensearchMethod}" template="/opensearch.xml" />
-            <Query role="example" searchTerms="Hearchco" />
-            <moz:SearchForm>${searchUrl}</moz:SearchForm>
-        </OpenSearchDescription>
-    `;
+<?xml version="1.0" encoding="utf-8"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
+	<ShortName>${instanceName}</ShortName>
+	<LongName>Hearchco metasearch engine</LongName>
+	<Description>Hearchco is a distributed and fast metasearch engine that respects your privacy.</Description>
+	<InputEncoding>UTF-8</InputEncoding>
+	<Image type="image/png">/favicon.png</Image>
+	${searchUrlTag}
+	<Url rel="self" type="application/opensearchdescription+xml" method="${opensearchMethod}" template="${instanceUrl}opensearch.xml" />
+	<Query role="example" searchTerms="Hearchco" />
+	<moz:SearchForm>${searchUrl}</moz:SearchForm>
+</OpenSearchDescription>
+	`.trim();
 
 	return body;
 }
