0ca6bb1d0b64618fa65d88c3704bee98787e3fc3
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-10 11:52:29 +0200

    fix(search): propagate server load to universal load

--- src/routes/search/+page.js
+++ src/routes/search/+page.js
@@ -1,8 +1,14 @@
 import { browser } from '$app/environment';
 
 /** @type {import('./$types').PageLoad} */
-export async function load() {
+export async function load({ data }) {
 	return {
-		browser: browser
+		browser: browser,
+		query: data.query,
+		currentPage: data.currentPage,
+		maxPages: data.maxPages,
+		category: data.category,
+		results: data.results,
+		timing: data.timing
 	};
 }
