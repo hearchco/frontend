d304944ddfd2a7f0040d34d6640403c9d5f44364
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-10 16:31:40 +0200

    feat: added simple JS theme toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
 	import '../app.css';
+	import ThemeToggle from '$lib/components/themetoggle/main.svelte';
 	import Footer from '$lib/components/footer/main.svelte';
 </script>
+
+<ThemeToggle />
 
 <main class="min-h-[90dvh]">
 	<!-- eslint-disable-next-line svelte/no-unused-svelte-ignore -->
 	<!-- svelte-ignore slot_element_deprecated -->
 	<slot />
 </main>
+
 <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'media',
+	darkMode: 'selector',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
