0e3b823b10d3a4dd104fc7c91e65377cdd694590
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-06-20 16:07:18 +0200

    fix(opensearch): small refactor and more customization

--- src/routes/opensearch.xml/+server.js
+++ src/routes/opensearch.xml/+server.js
@@ -1,6 +1,82 @@
 import { error } from '@sveltejs/kit';
 import { env } from '$env/dynamic/public';
 
+const opensearchType = 'application/opensearchdescription+xml';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET() {
+	const body = getOpensearchXml('GET');
+	return new Response(body, {
+		headers: {
+			'Content-Type': opensearchType
+		}
+	});
+}
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST() {
+	const body = getOpensearchXml('POST');
+	return new Response(body, {
+		headers: {
+			'Content-Type': opensearchType
+		}
+	});
+}
+
+/**
+ * @param {'GET' | 'POST'} opensearchMethod - The method to use for searching
+ * @returns {string} - The OpenSearch XML body
+ */
+function getOpensearchXml(opensearchMethod) {
+	const instanceName = env.PUBLIC_INSTANCE_NAME ?? 'Hearchco';
+	const instanceNameShort = instanceName.slice(
+		0,
+		instanceName.length > 16 ? 16 : instanceName.length
+	);
+
+	const description =
+		env.PUBLIC_DESCRIPTION ??
+		'Hearchco is a distributed and fast metasearch engine that respects your privacy.';
+	const descriptionShort = description.slice(
+		0,
+		description.length > 1024 ? 1024 : description.length
+	);
+
+	const faviconType = 'image/x-icon';
+	const faviconSize = 16;
+
+	const instanceUrl = getInstanceUrl();
+	const searchUrl = getSearchUrl();
+	const faviconUrl = getFaviconUrl();
+	// TODO: const suggestionsUrl = getSuggestionsUrl();
+
+	const searchUrlTag =
+		opensearchMethod === 'GET'
+			? `<Url rel="results" type="text/html" method="${opensearchMethod}" template="${searchUrl}?q={searchTerms}" />`
+			: `
+			<Url rel="results" type="text/html" method="${opensearchMethod}" template="${searchUrl}">
+				<Param name="q" value="{searchTerms}" />
+			</Url>
+			`;
+
+	// TODO: <Url rel="suggestions" type="application/x-suggestions+json" method="{{ opensearch_method }}" template="{{ url_for('autocompleter', _external=True) }}?q={searchTerms}"/>
+	const body = `
+<?xml version="1.0" encoding="utf-8"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
+	<ShortName>${instanceNameShort}</ShortName>
+	<Description>${descriptionShort}</Description>
+	<InputEncoding>UTF-8</InputEncoding>
+	<Image width="${faviconSize}" height="${faviconSize}" type="${faviconType}">${faviconUrl}</Image>
+	${searchUrlTag}
+	<Url rel="self" type="application/opensearchdescription+xml" method="${opensearchMethod}" template="${instanceUrl}opensearch.xml" />
+	<Query role="example" searchTerms="Hearchco" />
+	<moz:SearchForm>${searchUrl}</moz:SearchForm>
+</OpenSearchDescription>
+	`.trim();
+
+	return body;
+}
+
 /** @returns {string} */
 function getInstanceUrl() {
 	const instanceUri = env.PUBLIC_URI;
@@ -18,65 +94,13 @@ function getSearchUrl() {
 	return `${instanceUrl}search`;
 }
 
+/** @returns {string} */
+function getFaviconUrl() {
+	const instanceUrl = getInstanceUrl();
+	return `${instanceUrl}favicon.ico`;
+}
+
 // function getSuggestionsUrl() {
 // 	const instanceUrl = getInstanceUrl();
 // 	return `${instanceUrl}suggestions`;
 // }
-
-/**
- * @param {'GET' | 'POST'} opensearchMethod - The method to use for searching
- * @returns {string} - The OpenSearch XML body
- */
-function getOpenSearchXml(opensearchMethod) {
-	const instanceName = env.PUBLIC_INSTANCE_NAME ?? 'Hearchco';
-	const instanceUrl = getInstanceUrl();
-	const searchUrl = getSearchUrl();
-	// TODO: const suggestionsUrl = getSuggestionsUrl();
-
-	const searchUrlTag =
-		opensearchMethod === 'GET'
-			? `<Url rel="results" type="text/html" method="${opensearchMethod}" template="${searchUrl}?q={searchTerms}"/>`
-			: `<Url rel="results" type="text/html" method="${opensearchMethod}" template="${searchUrl}">
-				<Param name="q" value="{searchTerms}" />
-			</Url>`;
-
-	// TODO: <Url rel="suggestions" type="application/x-suggestions+json" method="{{ opensearch_method }}" template="{{ url_for('autocompleter', _external=True) }}?q={searchTerms}"/>
-	const body = `
-<?xml version="1.0" encoding="utf-8"?>
-<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-	<ShortName>${instanceName}</ShortName>
-	<LongName>Hearchco metasearch engine</LongName>
-	<Description>Hearchco is a distributed and fast metasearch engine that respects your privacy.</Description>
-	<InputEncoding>UTF-8</InputEncoding>
-	<Image type="image/svg">/favicon.svg</Image>
-	${searchUrlTag}
-	<Url rel="self" type="application/opensearchdescription+xml" method="${opensearchMethod}" template="${instanceUrl}opensearch.xml" />
-	<Query role="example" searchTerms="Hearchco" />
-	<moz:SearchForm>${searchUrl}</moz:SearchForm>
-</OpenSearchDescription>
-	`.trim();
-
-	return body;
-}
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET() {
-	const opensearchMethod = 'GET';
-	const body = getOpenSearchXml(opensearchMethod);
-	return new Response(body, {
-		headers: {
-			'Content-Type': 'application/opensearchdescription+xml'
-		}
-	});
-}
-
-/** @type {import('./$types').RequestHandler} */
-export async function POST() {
-	const opensearchMethod = 'POST';
-	const body = getOpenSearchXml(opensearchMethod);
-	return new Response(body, {
-		headers: {
-			'Content-Type': 'application/opensearchdescription+xml'
-		}
-	});
-}
