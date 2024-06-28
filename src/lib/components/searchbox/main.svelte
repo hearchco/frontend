e8b179605a9e008104e0b82bb2a9623033e0fc3c
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-06-28 12:40:15 +0200

    fix(suggestions): focusing properly

--- src/lib/components/searchbox/main.svelte
+++ src/lib/components/searchbox/main.svelte
@@ -19,13 +19,40 @@
 		loading = $bindable(false)
 	} = $props();
 
+	/** @type {HTMLElement | null} */
+	let searchBox = $state(null);
+	/** @type {HTMLElement | null} */
+	let searchInput = $state(null);
+
+	let currentIndex = $state(-1);
+	let clickedIndex = $state(-1);
+
+	let ableToShowSuggs = $state(true);
+	let suggestionsLength = $state(0);
+
+	/** @type {boolean} */
+	let showSuggestions = $derived(ableToShowSuggs && suggestionsLength > 0);
+
 	/** @param {SubmitEvent} e */
-	function handleSubmit(e) {
+	async function handleSubmit(e) {
 		if (query === '') {
 			e.preventDefault();
-		} else if (!loading) {
-			loading = true;
+			return;
 		}
+
+		if (clickedIndex !== -1) {
+			const suggs = await suggestions;
+			query = suggs[clickedIndex].value;
+		} else if (currentIndex !== -1) {
+			const suggs = await suggestions;
+			query = suggs[currentIndex].value;
+		}
+
+		loading = true;
+		clickedIndex = -1;
+		currentIndex = -1;
+		suggestionsLength = 0;
+		ableToShowSuggs = false;
 	}
 
 	function handleCategory() {
@@ -34,11 +61,8 @@
 
 	function handleReset() {
 		query = '';
-		document.getElementById('search-input')?.focus();
+		searchInput?.focus();
 	}
-
-	/** @type {number} */
-	let currentIndex = $state(-1);
 
 	/** @param {KeyboardEvent} event */
 	async function handleKeyDown(event) {
@@ -50,19 +74,10 @@
 			} else if (event.key === 'ArrowUp') {
 				currentIndex = Math.max(currentIndex - 1, -1);
 			}
-		} else if (event.key === 'Enter' && currentIndex !== -1) {
-			query = (await suggestions)[currentIndex].value;
-			ableToShowSuggs = false;
-		} else {
+		} else if (event.key !== 'Enter') {
 			currentIndex = -1;
 		}
 	}
-
-	let ableToShowSuggs = $state(false);
-	let suggestionsLength = $state(0);
-	let showSuggestions = $derived(
-		(!homepage || !loading) && ableToShowSuggs && suggestionsLength > 0
-	);
 
 	/** @type {Promise<SuggestionType[]>} */
 	let suggestions = $derived.by(async () => {
@@ -72,22 +87,38 @@
 		} else {
 			suggestionsLength = 0;
 			const suggs = await fetchSuggestions(query);
-			suggestionsLength = suggs.length;
 			if (suggs.length > 10) suggs.splice(10, suggs.length - 10);
+			suggestionsLength = suggs.length;
 			return suggs;
 		}
 	});
 </script>
 
+<svelte:window
+	onkeydown={({ key }) => {
+		if (key === 'Escape') {
+			searchInput?.blur();
+			ableToShowSuggs = false;
+		}
+	}}
+	onclick={({ target }) => {
+		if (target && target instanceof HTMLElement) {
+			if (searchBox && !searchBox.contains(target)) {
+				searchInput?.blur();
+				ableToShowSuggs = false;
+			}
+		}
+	}}
+/>
+
 <form
 	class:pt-5={category}
 	class="px-1 size-full"
-	id="search-form"
 	method="get"
 	action="/search"
 	onsubmit={handleSubmit}
 >
-	<div class="relative h-12 flex flex-col">
+	<div bind:this={searchBox} class="relative h-12 flex flex-col">
 		<div
 			class:shadow-2xl={homepage}
 			class:shadow-lg={!homepage}
@@ -99,10 +130,9 @@
 		>
 			<!-- svelte-ignore a11y_autofocus -->
 			<input
+				bind:this={searchInput}
 				onfocusin={() => (ableToShowSuggs = true)}
-				onfocusout={() => (ableToShowSuggs = false)}
 				onkeydown={handleKeyDown}
-				id="search-input"
 				name="q"
 				class="ml-4 mr-1.5 h-full w-full bg-transparent focus:outline-none"
 				type="text"
@@ -115,23 +145,24 @@
 				autofocus={homepage}
 				bind:value={query}
 			/>
-			<button
-				type="reset"
-				class:hidden={query === ''}
-				class="mx-1.5 max-5xs:hidden text-neutral-500 hover:text-hearchco-primary hover:dark:text-hearchco-secondary duration-100 ease-in-out"
-				onclick={handleReset}
-			>
-				<svg class="size-6" viewBox="0 0 512 512" aria-hidden="true">
-					<path
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="32"
-						d="M368 368L144 144M368 144L144 368"
-					></path>
-				</svg>
-			</button>
+			{#if query !== ''}
+				<button
+					type="reset"
+					class="mx-1.5 max-5xs:hidden text-neutral-500 hover:text-hearchco-primary hover:dark:text-hearchco-secondary duration-100 ease-in-out"
+					onclick={handleReset}
+				>
+					<svg class="size-6" viewBox="0 0 512 512" aria-hidden="true">
+						<path
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="32"
+							d="M368 368L144 144M368 144L144 368"
+						></path>
+					</svg>
+				</button>
+			{/if}
 			<button
 				type="submit"
 				name="category"
@@ -171,18 +202,14 @@
 							class:pb-1={i === suggestions.length - 1}
 							class:bg-neutral-100={currentIndex === i}
 							class:dark:bg-neutral-700={currentIndex === i}
-							class="rounded-lg hover:bg-neutral-100 dark:hover:bg-neutral-700"
+							class="rounded-lg"
 						>
 							<button
 								onmouseover={() => (currentIndex = i)}
 								onfocus={() => (currentIndex = i)}
-								onmousedown={() => {
-									query = suggestion.value;
-									ableToShowSuggs = false;
-								}}
-								class="px-4 py-0.5 size-full text-left"
+								onclick={() => (clickedIndex = i)}
 								type="submit"
-								value={suggestion.value}
+								class="px-4 py-0.5 size-full text-left"
 							>
 								{suggestion.value}
 							</button>
