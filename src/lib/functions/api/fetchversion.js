e246caf05d9f547ca81be1f63f26e50387f5ce6e
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-10-22 22:56:02 +0200

    fix(functions/api): add case for not OK resp

--- src/lib/functions/api/fetchversion.js
+++ src/lib/functions/api/fetchversion.js
@@ -27,7 +27,24 @@ export async function fetchVersion(fetcher = fetch) {
 		});
 	} catch (err) {
 		// Bad Gateway.
-		throw error(502, `Failed to fetch version: ${err}`);
+		throw error(502, `Failed to fetch: ${err}`);
+	}
+
+	// Check if the response is OK, otherwise handle the error.
+	if (!response.ok) {
+		// Try to read the response body as text.
+		let responseBody;
+		try {
+			responseBody = await response.text();
+		} catch (err) {
+			responseBody = `Failed to read response body: ${err}`;
+		}
+
+		// Same as the API.
+		throw error(
+			response.status,
+			`Response from API not OK (${response.statusText}): ${responseBody}`
+		);
 	}
 
 	/** @type {string} */
@@ -35,8 +52,8 @@ export async function fetchVersion(fetcher = fetch) {
 	try {
 		version = await response.text();
 	} catch (err) {
-		// Internal Server Error.
-		throw error(500, `Failed to read version: ${err}`);
+		// Bad Gateway.
+		throw error(502, `Failed to read response body: ${err}`);
 	}
 
 	return version;
